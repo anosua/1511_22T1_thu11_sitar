5fafb102ad3e28e009fa526fb07e7812952b4af0
Anosua Roy <64733993+anosua@users.noreply.github.com>
2022-03-03 21:59:39 +1100

    Create if.c

---
+++ W02/if.c
@@ -0,0 +1,28 @@
+// If statement example
+// Anosua Roy z5264396
+// 2022-Feb-22
+
+// 
+// if (condition) {
+//     code_statements;    
+// }
+//
+
+#include <stdio.h>
+
+int main(void) {
+    
+    if (5 > 10) {
+        printf("The if statement ran!\n");
+    } else if (10 == 10) {
+        
+        if (age > 5) {
+        
+        } else () {
+        
+        }
+        
+    }
+    
+    return 0;
+}
