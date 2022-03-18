a7bb642feb508f31c0819145553fc4683787d4b1
Anosua Roy <64733993+anosua@users.noreply.github.com>
2022-03-18 11:13:11 +1100

    Update stu_marks.c

--- W05/stu_marks.c
+++ W05/stu_marks.c
@@ -1,28 +1,29 @@
-// Live demo of pointers and functions
+// Live demo of 2D arrays
 // Anosua Roy z5264396
 // 2022-03-15
 
 #include <stdio.h>
 
-int *max(int *a, int *b);
+#define NUM_STU 3
+#define NUM_SUBJ 2
 
 int main(void) {
-    int my_number = 100;
-    int your_number = 2;
-    
-    // TODO: call the function here
-    int *result = max(&my_number, &your_number);
-    
-    printf("result = %p\n", result);
-    printf("value at %p: %d\n", result, *result);
-    
-    return 0;
-}
 
-int *max(int *a, int *b) {
-    if (*a > *b) {
-        return a;
+    int student_marks[NUM_STU][NUM_SUBJ] = {{100, 98}, {56, 54}, {2, 4}};
+    
+    int curr_stu = 0;
+    while (curr_stu < NUM_STU) {
+        printf("student %d: ", curr_stu);
+        int curr_subj = 0;
+        while (curr_subj < NUM_SUBJ) {
+            
+            printf("%d ", student_marks[curr_stu][curr_subj]);
+            
+            curr_subj++;
+        }
+        printf("\n");
+        curr_stu++;
     }
     
-    return b;
+    return 0;
 }
