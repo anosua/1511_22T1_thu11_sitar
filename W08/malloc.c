993903e6c6e75823b7113c9f4235c5de495a5352
Anosua Roy <64733993+anosua@users.noreply.github.com>
2022-04-07 12:44:14 +1000

    Create malloc.c

---
+++ W08/malloc.c
@@ -0,0 +1,14 @@
+// Live demo of using malloc
+// Anosua Roy z5264396
+// 2022/04/07
+
+#include <stdio.h>
+
+int main(void) {
+    
+    // Create an "int *" variable that points to memory allocated using malloc
+    int *pointer = malloc(sizeof(int));
+
+
+    return 0;
+}
